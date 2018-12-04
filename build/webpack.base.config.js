e4523861a7a7e35b26c656e725d04da0a38ad231
d1313113 <cumelmell@foxmail.com>
2018-12-04 20:15:43 +0800

    添加热更新,webpack-dev-server

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "/dist")
-  }
+  },
+  devServer: {
+    contentBase: "../dist", //本地服务器所加载页面的目录
+    historyApiFallback: true, //不跳转
+    inline: true, // 实时更新
+    port: 3333, // 端口
+    open: true, //自动打开浏览器
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 document.querySelector("#root").innerHTML = "webpack使用";
+
+if (module.hot) {
+  // 开启热更新
+  module.hot.accept();
+}
